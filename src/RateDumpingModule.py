cecaece447e57cb823a4f7ce4dce68b4c0079b90
donkhan <donkhan@stash.maxmoney.com>
2018-03-12 13:29:38 +0530

    Fixed some issues

--- src/RateDumpingModule.py
+++ src/RateDumpingModule.py
@@ -7,23 +7,16 @@ from datetime import datetime
 stocks=[]
 stocks.append("GMRINFRA")
 stocks.append("RICOAUTO")
-#stocks.append("SUNPHARMA")
 stocks.append("RELINFRA")
-#stocks.append("TATAMOTORS")
 stocks.append("BATAINDIA")
 stocks.append("GLENMARK")
-#stocks.append("ONGC")
 
 mcstocks = []
 mcstocks.append({ 'url' : "http://www.moneycontrol.com/india/stockpricequote/auto-ancillaries/ricoauto/RA04", 'name' : 'RICO'})
 mcstocks.append({ 'url' : "http://www.moneycontrol.com/india/stockpricequote/auto-ancillaries/ricoauto/RA04", 'name' : 'RICO'})
 
 
-
-#https://www.nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuote.jsp?symbol=GMRINFRA
-
-
-def nse(args):
+def mc(args):
     _pass = 0
     while _pass < 7*60:
         t = str(datetime.now())
@@ -37,7 +30,7 @@ def nse(args):
             _pass = _pass + 1
         time.sleep(60)
 
-def mc(args):
+def nse(args):
     _pass = 0
     while _pass < 7*60:
         t = str(datetime.now())
